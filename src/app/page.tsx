aac7c20742c139cdc5c15d85c56c968f7668f664
akkun <akkun@akkunnoMacBook-Air.local>
2024-11-14 01:13:52 +0900

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import PriorRegistration from "./components/PriorRegistration";
 
 export default function Home() {
